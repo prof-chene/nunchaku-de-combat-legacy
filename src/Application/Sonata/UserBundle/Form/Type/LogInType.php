fa83b24d6097e74f56972e40cd5267f2d8ae9b9c
Prof. Chêne <alexandre.duch3ne@gmail.com>
2018-01-07 23:45:06 +0100

    fix .gitignore fix typo

--- src/Application/Sonata/UserBundle/Form/Type/LogInType.php
+++ src/Application/Sonata/UserBundle/Form/Type/LogInType.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
- * Class LoginType
+ * Class LogInType
  * @package Application\Sonata\Form
  */
 class LogInType extends AbstractType
