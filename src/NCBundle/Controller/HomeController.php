65581568dd5df685fe8026831ab23c8c4af61c57
Prof. Chêne <alexandre.duch3ne@gmail.com>
2018-08-19 11:50:51 +0200

    Add trailing slashes to routes config This ensures that both http://mydomain.com/page and http://mydomain.com/page/ will work, while avoiding duplicate content @see https://symfony.com/doc/3.4/routing.html#redirecting-urls-with-trailing-slashes

--- src/NCBundle/Controller/HomeController.php
+++ src/NCBundle/Controller/HomeController.php
@@ -4,6 +4,8 @@ namespace NCBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class HomeController
@@ -12,10 +14,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class HomeController extends Controller
 {
     /**
+     * @param Request $request
+     *
      * @Template
+     *
+     * @return array|RedirectResponse
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        /* This redirects https://mydomain.com/DEFAULT_LOCALE to https://mydomain.com to have a prettier homepage url
+           and avoid duplicate-content */
+        if ($request->attributes->get('_locale') === $this->getParameter('locale')) {
+            return $this->redirectToRoute('homepage', [], 301);
+        }
+
         return array();
     }
 }
