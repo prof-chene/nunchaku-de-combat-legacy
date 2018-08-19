a0fddebccb4c56d069a70a3c5bdef33e71e8729f
Prof. Chêne <alexandre.duch3ne@gmail.com>
2018-08-19 20:09:01 +0200

    Fix logout redirect ignoring locale

---
+++ src/NCBundle/Event/AuthenticationListener.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace NCBundle\Event;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
+
+/**
+ * Class AuthenticationListener
+ *
+ * @package NCBundle\Event
+ */
+class AuthenticationListener implements LogoutSuccessHandlerInterface
+{
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * AuthenticationListener constructor.
+     *
+     * @param RouterInterface $router
+     */
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function onLogoutSuccess(Request $request)
+    {
+        return new RedirectResponse(
+            $this->router->generate('homepage_localized', ['_locale' => $request->getLocale()])
+        );
+    }
+}
